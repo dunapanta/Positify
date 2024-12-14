26ed7696e12a98f93687e18e8a83ddcbd4a8d6a2
Daniel Unapanta <dunapanta@outlook.es>
2024-12-14 16:53:00 -0500

    Add affirmations structure

---
+++ src/interfaces/affirmationsInterface.ts
@@ -0,0 +1,10 @@
+export interface Affirmation {
+    id: string;
+    affirmationEN: string;
+    affirmationES: string;
+    isFavorite: boolean;
+    category: string;
+    createdBy: string;
+    createdAt: string;
+    updatedAt: string;
+}
