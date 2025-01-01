9467ac7d00cdb7ac270247e45bb2a09c08911f5a
Daniel Unapanta <dunapanta@outlook.es>
2025-01-01 13:36:09 -0500

    Add abundance mindset audios

--- src/constants/audios.ts
+++ src/constants/audios.ts
@@ -1,9 +1,13 @@
 const generalAudioEN = require("@/src/assets/audios/en_affirmation_general.m4a");
 const generalAudioES = require("@/src/assets/audios/es_affirmation_general.m4a");
+const ambundanceMindsetAudioES = require("@/src/assets/audios/es_abundance_mindset.m4a");
+const ambundanceMindsetAudioEN = require("@/src/assets/audios/en_abundance_mindset.m4a");
 const ambientInnerHarmony = require("@/src/assets/audios/inner_harmony.m4a");
 
 export default {
     generalAudioEN,
     generalAudioES,
     ambientInnerHarmony,
+    ambundanceMindsetAudioES,
+    ambundanceMindsetAudioEN
 }
